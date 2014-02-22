a6c6bbbb7b8aa3e6f9ee21582802e6e7abb1fcf6
SPOT-2 <dane.t.larsen@gmail.com>
2014-02-22 21:21:30 +0000

    set rosblinker to listen for the task list messages

--- ros/rosblinker.py
+++ ros/rosblinker.py
@@ -7,20 +7,20 @@ import rospy
 from std_msgs.msg import String
 
 def callback(data):
-	if data.data == "0":
-		print "Lights off"
-		spot_gpio.set_pin(8, False)
-	elif data.data == "1":
-		print "Lights on"
-		spot_gpio.set_pin(8, False)
-	else:
-		print "Bad data: '%s'" % data.data
+    if data.data == "task: lights off":
+        print "Lights off"
+        spot_gpio.set_pin(8, False)
+    elif data.data == "task: lights on":
+        print "Lights on"
+        spot_gpio.set_pin(8, True)
+    else:
+        print "Bad data: '%s'" % data.data
 
 def listener():
-	rospy.init_node("listener", anonymous=True)
-	rospy.Subscriber("chatter", String, callback)
-	print "listening..."
-	rospy.spin()
+    rospy.init_node("rosblinker", anonymous=True)
+    rospy.Subscriber("spot_tasks", String, callback)
+    print "listening..."
+    rospy.spin()
 
 if __name__ == "__main__":
-	listener()
+    listener()
