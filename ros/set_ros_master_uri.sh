eb4fc5f4be6a8d29a58d56faa0b09737c977fd06
SPOT-4 <dane.t.larsen@gmail.com>
2014-02-28 00:09:11 +0000

    prevent ros master uri script from killing session

--- ros/set_ros_master_uri.sh
+++ ros/set_ros_master_uri.sh
@@ -5,10 +5,8 @@ echo "Are you sourcing this script? If so, good. If not, run 'source $0 $1'"
 if [[ $1 != "spot-1" && $1 != "spot-2" && $1 != "spot-3" && $1 != "spot-4" ]]
 then
 	echo "Error! First argument must be 'spot-[1-4]'"
-	exit 1
+else
+	url="http://pcduino-ips.larsendt.com/$1"
+	ip=$(wget -qO - $url)
+	export ROS_MASTER_URI="http://$ip:11311"
 fi
-
-url="http://pcduino-ips.larsendt.com/$1"
-ip=$(wget -qO - $url)
-
-export ROS_MASTER_URI="http://$ip:11311"
