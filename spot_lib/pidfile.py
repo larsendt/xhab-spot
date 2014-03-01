18d16863cc26edc3a72bff158e410425f1d70f09
SPOT-2 <dane.t.larsen@gmail.com>
2014-03-01 17:44:05 +0000

    added pidfile library

---
+++ spot_lib/pidfile.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import psutil
+import os
+import time
+import json
+
+PID_DIR = "/home/xhab/pidfiles"
+
+def pid_filename(class_name, pid):
+    return os.path.join(PID_DIR, class_name + "." + str(pid) + ".pid")
+
+def create_pidfile(class_name):
+    timestamp = time.time()
+    pid = os.getpid()
+    obj = {"pid":pid, "time":timestamp, "name":class_name}
+
+    if not os.path.exists(PID_DIR):
+        os.makedirs(PID_DIR)
+
+    pid_fname = pid_filename(class_name, pid)
+    with open(pid_fname, "w") as f:
+        json.dump(obj, f)
+
+    return pid_fname
+
+
+def get_pids(active):
+    pids = []
+    for fname in os.listdir(PID_DIR):
+        with open(fname, "r") as f:
+            obj = json.load(f)
+
+        try:
+            proc = psutil.Process(obj["pid"])
+            if active:
+                pids.append((obj["pid"], obj["name"]))
+        except NoSuchProcess:
+            if not active:
+                pids.append((obj["pid"], obj["name"]))
+
+    return pids
+
+
+def get_active_class_pids(class_name):
+    active_pids = get_pids(True)
+    return filter(lambda x: x[1] == class_name, active_pids)
+
+
+def get_pidinfo(class_name, pid):
+    pid_fname = pid_filename(class_name, pid)
+    if os.path.exists(pid_fname):
+        with open(pid_fname, "r") as f:
+            return json.load(f)
+    else:
+        return None
+
+
+
+
