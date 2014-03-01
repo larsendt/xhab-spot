929b2b07b474227cd1d3b691747995e12cc3a047
SPOT-2 <dane.t.larsen@gmail.com>
2014-03-01 21:02:26 +0000

    added lib for getting spot name

---
+++ spot_lib/identity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+def get_spot_name():
+    with open("/etc/hostname", "r") as f:
+        hostname = f.read().replace("\n", "")
+    return hostname
+
+if __name__ == "__main__":
+    print get_spot_name()
