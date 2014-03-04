73ea82cdbecd91576e4025027d4508e618a37f50
SPOT-2 <dane.t.larsen@gmail.com>
2014-03-04 22:46:53 +0000

    scheduler now follows topic format

--- catkin_ws/src/xhab_spot/src/nodes/scheduler.py
+++ catkin_ws/src/xhab_spot/src/nodes/scheduler.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 import rospy
 import time
+from xhab_spot.msg import Scheduler
 from std_msgs.msg import String
+import identity
 
 class Scheduler(object):
     def __init__(self):
-        self.pub = rospy.Publisher("scheduled_tasks", String)
-        rospy.init_node("PeriodicScheduler")
+        pub_topic = "/tasks/" + identity.get_spot_name() + "/scheduled"
+        self.pub = rospy.Publisher(pub_topic, String)
+        rospy.init_node("Scheduler")
 
     def spin(self):
         while not rospy.is_shutdown():
