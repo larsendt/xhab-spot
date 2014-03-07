1b644175976ada94313b4472396add50b7fdb384
SPOT-2 <dane.t.larsen@gmail.com>
2014-03-07 23:21:24 +0000

    Working Ph sensor code

--- sandbox/PHSensor/phsensor.py
+++ sandbox/PHSensor/phsensor.py
@@ -1,13 +1,20 @@
 import serial ##Load the serial library
 
 ## Select and configure the port
-myPort = serial.Serial('/dev/ttyS1', 115200, bytesize=8, parity='N', stopbits=1, timeout = 10)
+myPort = serial.Serial('/dev/ttyS1', 38400, bytesize=8, parity='N', stopbits=1, timeout = 10)
 
-##Wait for data to come in - one byte only
-ph_volt = myPort.read()
+ph = ""
 
-##Echo the data to the command prompt
-print "Ph voltage = " + ph_volt
+while(1):
+    ##Wait for data to come in - one byte only
+    inputchar = myPort.read()
+    ph += inputchar
+    if(inputchar == '\r'):
+        ##Echo the data to the command prompt
+        print "Ph value = " + ph
+        ##Clear the string
+        inputchar = ""
+        ph = ""
 
 ##Close the port
 myPort.close()
