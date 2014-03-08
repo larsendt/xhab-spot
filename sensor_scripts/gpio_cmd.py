c962d3cabb26eaa7ae6c71b725590f253dfe6977
SPOT-2 <dane.t.larsen@gmail.com>
2014-03-08 18:20:27 +0000

    added a read with pullup option (readpullup) to spot_gpio and gpio_cmd

--- sensor_scripts/gpio_cmd.py
+++ sensor_scripts/gpio_cmd.py
@@ -6,50 +6,54 @@ import time
 
 # read
 if len(sys.argv) == 3:
-	mode = sys.argv[1]
-	if mode == "write":
-		print "Must provide write value"
-		sys.exit(1)
-	elif mode != "read":
-		print "Unknown mode:", mode
-		sys.exit(1)
-	pin = int(sys.argv[2])
-	if not (0 <= pin <= 23):
-		print "Pin must be between 0 and 23"
-		sys.exit(1)
-	spot_gpio.set_pin_mode(pin, spot_gpio.WRITE)	
-	print "Reading from %s" % spot_gpio.pin_path(pin)
-	while True:
-		print spot_gpio.get_pin(pin)
-		time.sleep(0.5)
+    mode = sys.argv[1]
+    if mode == "write":
+        print "Must provide write value"
+        sys.exit(1)
+    elif mode != "read" and mode != "readpullup":
+        print "Unknown mode:", mode
+        sys.exit(1)
+    pin = int(sys.argv[2])
+    if not (0 <= pin <= 23):
+        print "Pin must be between 0 and 23"
+        sys.exit(1)
+    #spot_gpio.set_pin_mode(pin, spot_gpio.WRITE)    
+    print "Reading from %s" % spot_gpio.pin_path(pin)
+    while True:
+        if mode == "read":
+            print spot_gpio.get_pin(pin)
+        elif mode == "readpullup":
+            print spot_gpio.get_pin_pullup(pin)
+        time.sleep(0.5)
+
 #write
 elif len(sys.argv) == 4:
-	mode = sys.argv[1]
-	pin = int(sys.argv[2])
-	value = sys.argv[3]
+    mode = sys.argv[1]
+    pin = int(sys.argv[2])
+    value = sys.argv[3]
 
-	if mode == "read":
-		print "Read only takes 2 arguments"
-		sys.exit(1)
-	elif mode != "write":
-		print "Unknown mode:", mode
-		sys.exit(1)
+    if mode == "read" or mode == "readpullup":
+        print "Read only takes 2 arguments"
+        sys.exit(1)
+    elif mode != "write":
+        print "Unknown mode:", mode
+        sys.exit(1)
 
-	if not (0 <= pin <= 23):
-		print "Pin must be between 0 and 23"
-		sys.exit(1)
+    if not (0 <= pin <= 23):
+        print "Pin must be between 0 and 23"
+        sys.exit(1)
 
-	if not (value == "0" or value == "1"):
-		print "Value must be 0 or 1"
-		sys.exit(1)
+    if not (value == "0" or value == "1"):
+        print "Value must be 0 or 1"
+        sys.exit(1)
 
-	print "Writing %s to %s" % (value, spot_gpio.pin_path(pin))
-	spot_gpio.set_pin_mode(pin, spot_gpio.READ)
-	if value == "1":
-		spot_gpio.set_pin(pin, True)
-	else:
-		spot_gpio.set_pin(pin, False)
-		
+    print "Writing %s to %s" % (value, spot_gpio.pin_path(pin))
+    spot_gpio.set_pin_mode(pin, spot_gpio.READ)
+    if value == "1":
+        spot_gpio.set_pin(pin, True)
+    else:
+        spot_gpio.set_pin(pin, False)
+        
 
 else:
-	print "Usage: %s <read,write> <pin> [0,1]"
+    print "Usage: %s <read,readpullup,write> <pin> [0,1]"
--- sensor_scripts/spot_gpio.py
+++ sensor_scripts/spot_gpio.py
@@ -12,45 +12,53 @@ PIN_PATH = "/sys/devices/virtual/misc/gpio/pin/"
 
 READ = "0"
 WRITE = "1"
+READ_PULLUP = "8"
 OFF = "0"
 ON = "1"
 
 def set_pin_mode(pin, mode):
-	if 0 <= pin <= 23:
-		fname = MODE_PATH + "/gpio" + str(pin)
-		with open(fname, "w") as f:
-			f.write(mode)
-	else:
-		raise ValueError("Pin must be between 0 and 23 (inclusive)")
+    if 0 <= pin <= 23:
+        fname = MODE_PATH + "/gpio" + str(pin)
+        with open(fname, "w") as f:
+            f.write(mode)
+    else:
+        raise ValueError("Pin must be between 0 and 23 (inclusive)")
 
 def set_pin(pin, on):
-	if 0 <= pin <= 23:
-		set_pin_mode(pin, WRITE)
-		fname = PIN_PATH + "/gpio" + str(pin)
-		with open(fname, "w") as f:
-			if on:
-				print "wrote ON"
-				f.write(ON)
-			else:
-				print "wrote OFF"
-				f.write(OFF)
-	else:
-		raise ValueError("Pin must be between 0 and 23 (inclusive)")
+    if 0 <= pin <= 23:
+        set_pin_mode(pin, WRITE)
+        fname = PIN_PATH + "/gpio" + str(pin)
+        with open(fname, "w") as f:
+            if on:
+                print "wrote ON"
+                f.write(ON)
+            else:
+                print "wrote OFF"
+                f.write(OFF)
+    else:
+        raise ValueError("Pin must be between 0 and 23 (inclusive)")
 
 def get_pin(pin):
-	if 0 <= pin <= 23:
-		set_pin_mode(pin, READ)
-		fname = PIN_PATH + "/gpio" + str(pin)
-		with open(fname, "r") as f:
-			return int(f.read())
+    if 0 <= pin <= 23:
+        set_pin_mode(pin, READ)
+        fname = PIN_PATH + "/gpio" + str(pin)
+        with open(fname, "r") as f:
+            return int(f.read())
 
-	else:
-		raise ValueError("Pin must be between 0 and 23 (inclusive)")
+    else:
+        raise ValueError("Pin must be between 0 and 23 (inclusive)")
+
+def get_pin_pullup(pin):
+    if 0 <= pin <= 23:
+        set_pin_mode(pin, READ_PULLUP)
+        fname = PIN_PATH + "/gpio" + str(pin)
+        with open(fname, "r") as f:
+            return int(f.read())
 
 def pin_path(pin):
-	if 0 <= pin <= 23:
-		return PIN_PATH + "/gpio" + str(pin)
-	else:
-		raise ValueError("Pin must be between 0 and 23 (inclusive)")
-			
+    if 0 <= pin <= 23:
+        return PIN_PATH + "/gpio" + str(pin)
+    else:
+        raise ValueError("Pin must be between 0 and 23 (inclusive)")
+            
 
