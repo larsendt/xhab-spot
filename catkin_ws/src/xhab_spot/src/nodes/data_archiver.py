c2360e9bc62108e802a2dda9cfeea266fda0d343
SPOT-2 <dane.t.larsen@gmail.com>
2014-03-09 00:11:44 +0000

    data archiver now saves to sqlite

--- catkin_ws/src/xhab_spot/src/nodes/data_archiver.py
+++ catkin_ws/src/xhab_spot/src/nodes/data_archiver.py
@@ -2,20 +2,45 @@
 import rospy
 import time
 import sqlite3
+import os
 from std_msgs.msg import String
 import spot_topics
 import identity
 
+DB_DIR = "/home/xhab/data"
+DB_FILE = os.path.join(DB_DIR, "archive.sqlite")
+
+def init_database():
+    print "initializing database:", DB_FILE
+    if not os.path.exists(DB_DIR):
+        os.makedirs(DB_DIR)
+
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS spot_archive (source TEXT, property TEXT, timestamp INTEGER, value REAL)")
+    conn.commit()
+    conn.close()
+
+def insert_data(source, property, timestamp, value):
+    values = (source, property, timestamp.secs, value)
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+    c.execute("INSERT INTO spot_archive VALUES (?,?,?,?)", values)
+    conn.commit()
+    conn.close()
+
 class DataArchiver(object):
     def __init__(self):
         print "DataArchiver init"
+        init_database()
         rospy.init_node("DataArchiver")
         subtopic = "/data/" + identity.get_spot_name() 
         self.subscribers = spot_topics.make_data_subscribers(subtopic, self.data_callback) 
-        self.archive = []
 
     def data_callback(self, message):
         print "archiver got:", message 
+        insert_data(message.source, message.property, message.timestamp, message.value)
+        print "inserted data"
 
     def spin(self):
         print "DataArchiver listening"
