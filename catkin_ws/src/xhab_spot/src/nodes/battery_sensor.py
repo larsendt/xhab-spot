5ad7b8f3e0f014d480a0d6bb7e6a177c7078e8b8
SPOT-2 <dane.t.larsen@gmail.com>
2014-03-15 19:40:13 +0000

    battery sensor working with fake data

--- catkin_ws/src/xhab_spot/src/nodes/battery_sensor.py
+++ catkin_ws/src/xhab_spot/src/nodes/battery_sensor.py
@@ -4,8 +4,10 @@ import sys
 import rospy
 from xhab_spot.msg import *
 import identity
-
+import time
 import random
+
+PUB_DELAY = 15
 
 class BatterySensor(object):
     def __init__(self):
@@ -15,25 +17,36 @@ class BatterySensor(object):
         pubtopic = "/data/" + identity.get_spot_name() + "/battery"
         self.pub = rospy.Publisher(pubtopic, Data)
         self.sub = rospy.Subscriber(subtopic, BatteryTask, self.callback)
+        self.charging = True
+        self.level = 0.51
+        self.full = False
 
     def callback(self, msg):
         print "got msg, target =", msg.target
-        reading = 0.75
-        pubmsg = Data()
-        pubmsg.source = identity.get_spot_name()
-        pubmsg.property = "battery_level"
-        pubmsg.timestamp = rospy.Time.now()
-        pubmsg.value = reading
-        self.pub.publish(pubmsg)
-
-        pubmsg.property = "battery_charging"
-        pubmsg.value = random.choice((1.0, 0.0))
-        self.pub.publish(pubmsg)
-        print "published 2 messages"
+        self.charging = random.choice((True, False))
+        self.level = self.level + 0.01 if self.charging else self.level - 0.01
+        self.level = min(1.0, self.level)
+        self.full = True if self.level >= 1.0 else False
 
     def spin(self):
         print "BatterySensor listening"
-        rospy.spin()
+        while not rospy.is_shutdown():
+            pubmsg = Data()
+            pubmsg.source = identity.get_spot_name()
+            pubmsg.property = "battery_level"
+            pubmsg.timestamp = rospy.Time.now()
+            pubmsg.value = self.level
+            self.pub.publish(pubmsg)
+
+            pubmsg.property = "battery_charging"
+            pubmsg.value = 1.0 if self.charging else 0.0
+            self.pub.publish(pubmsg)
+
+            pubmsg.property = "battery_full"
+            pubmsg.value = 1.0 if self.full else 0.0
+            self.pub.publish(pubmsg)
+            print "published"
+            time.sleep(PUB_DELAY)
 
 if __name__ == "__main__":
     try:
