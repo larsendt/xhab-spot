8d3166da33145ed53c355e0d832739235bd6c98d
SPOT-2 <dane.t.larsen@gmail.com>
2014-03-16 03:01:52 +0000

    lights controller can now disable the lights for some period of time

--- catkin_ws/src/xhab_spot/src/nodes/lcd_interface.py
+++ catkin_ws/src/xhab_spot/src/nodes/lcd_interface.py
@@ -11,6 +11,10 @@ import pyinotify
 
 ROTATE_PUB = rospy.Publisher("/tasks/" + identity.get_spot_name() + "/rotation", RotationTask)
 DOOR_PUB = rospy.Publisher("/tasks/" + identity.get_spot_name() + "/door", DoorTask)
+LIGHTS_PUB = rospy.Publisher("/tasks/" + identity.get_spot_name() + "/lights", LightsTask)
+VALVE_PUB = rospy.Publisher("/tasks/" + identity.get_spot_name() + "/valve", ValveTask)
+CAMERA_PUB = rospy.Publisher("/tasks/" + identity.get_spot_name() + "/camera", CameraTask)
+
 
 def rotate_90_cw():
     print "rotate_90_cw"
@@ -38,20 +42,98 @@ def rotate_180():
 
 def door_change_request():
     print "door_change_request"
+    with open("/home/xhab/data/door_status.txt", "r") as f:
+        status = f.read()[0]
 
-def lights_time_request():
-    print "lights_time_request"
+    if status == "1":
+        newstatus = 0.0
+        print "closing door"
+    else:
+        newstatus = 1.0
+        print "opening door"
 
-def lights_brightness_request():
-    print "lights_brightness_request"
+    msg = DoorTask()
+    msg.spot_id = identity.get_spot_name()
+    msg.timestamp = rospy.Time.now()
+    msg.open = newstatus
+
+def light_time_request():
+    print "light_time_request"
+    with open("/home/xhab/data/light_time_request.txt", "r") as f:
+        time = int(f.read().replace("\n", ""))
+
+    # clamp to (0, 480), so we don't cause problems
+    time = max(0, min(480, time))
+
+    if time > 0:
+        print "disabling lights for %d minutes" % time
+        msg = LightsTask()
+        msg.spot_id = identity.get_spot_name()
+        msg.timestamp = rospy.Time.now()
+        msg.disable_minutes = time
+        LIGHTS_PUB.publish(msg)
+
+        with open("/home/xhab/data/light_time_request.txt", "w") as f:
+            f.write("0\n")
+
+def light_brightness_request():
+    print "light_brightness_request"
+    msg = LightsTask()
+    msg.spot_id = identity.get_spot_name()
+    msg.timestamp = rospy.Time.now()
+
+    with open("/home/xhab/data/light_brightness_request.txt", "r") as f:
+        brightness = min(255, int(f.read().replace("\n", "")))
+    brightness /= 255.0
+
+    msg.whites_on = True
+    msg.reds_on = True
+
+    print "setting brightness to %.2f" % brightness
+    msg.brightness = brightness
+    LIGHTS_PUB.publish(msg)
+
+def drain_spot_request():
+    print "drain_spot_request"
+    msg = ValveTask()
+    msg.spot_id = identity.get_spot_name()
+    msg.timestamp = rospy.Time.now()
+    msg.valve_open = True
+    print "opening valve"
+    VALVE_PUB.publish(msg)
+
+
+def take_picture_request():
+    print "take_picture_request"
+    msg = CameraTask()
+    msg.spot_id = identity.get_spot_name()
+    msg.timestamp = rospy.Time.now()
+    CAMERA_PUB.publish(msg)
+
+
+def lcd_status():
+    print "lcd_status"
+    with open("/home/xhab/data/lcd_status.txt", "r") as f:
+        text = f.read()[0]
+
+    if text == "1":
+        with open("/home/xhab/data/lcd_status.txt", "w") as f:
+            f.write("0\n")
+        print "NEED TO DO: sleep for 30 seconds"
+        #time.sleep(30)
+        print "NEED TO DO: lcd.lcd_off()"
+
 
 PATH = "/home/xhab/data/"
 FILES_TO_MONITOR = {"rotate_90_cw.txt":rotate_90_cw, 
                     "rotate_90_ccw.txt":rotate_90_ccw, 
                     "rotate_180.txt":rotate_180,
                     "door_change_request.txt":door_change_request,
-                    "lights_time_request.txt":lights_time_request,
-                    "lights_brightness_request.txt":lights_brightness_request}
+                    "light_time_request.txt":light_time_request,
+                    "light_brightness_request.txt":light_brightness_request,
+                    "drain_spot_request.txt":drain_spot_request,
+                    "take_picture_request.txt":take_picture_request,
+                    "lcd_status.txt":lcd_status}
 
 class EventProcessor(pyinotify.ProcessEvent):
     def __init__(self):
@@ -62,6 +144,8 @@ class EventProcessor(pyinotify.ProcessEvent):
         if event.name in FILES_TO_MONITOR:
             func = FILES_TO_MONITOR[event.name]
             func()
+        else:
+            print "unknown event name:", event.name
 
 
 def setup_notify():
--- catkin_ws/src/xhab_spot/src/nodes/light_controller.py
+++ catkin_ws/src/xhab_spot/src/nodes/light_controller.py
@@ -17,10 +17,14 @@ class LightController(object):
         subtopic = "/tasks/" + identity.get_spot_name() + "/lights"
         pubtopic = "/data/" + identity.get_spot_name() + "/lights"
         self.pub = rospy.Publisher(pubtopic, Data)
+        self.alert_pub = rospy.Publisher("/alerts/" + identity.get_spot_name(), Alert)
         self.sub = rospy.Subscriber(subtopic, LightsTask, self.callback)
         self.brightness = 0.75
         self.reds_on = False
         self.whites_on = True
+        self.disabled = False
+        self.disable_start = 0
+        self.disable_stop = 0
 
     def callback(self, msg):
         print "got msg, target =", msg.target
@@ -33,15 +37,40 @@ class LightController(object):
         self.reds_on = msg.reds_on
         print "reds on:", self.reds_on
 
+        if msg.disable_minutes > 0:
+            print "disabled for %d minutes" % msg.disable_minutes
+            seconds = msg.disable_minutes * 60
+            self.disable_start = time.time()
+            self.disable_stop = time.time() + seconds
+            self.disabled = True
+
+            alertmsg = Alert()
+            alertmsg.spot_id = identity.get_spot_name()
+            alertmsg.timestamp = rospy.Time.now()
+            alertmsg.alert_text = "Lights disabled for %d minutes" % msg.disable_minutes
+            self.alert_pub.publish(alertmsg)
+        else:
+            if self.disabled:
+                print "lights no longer disabled!"
+            self.disabled = False
+
 
     def spin(self):
         print "LightController listening"
         while not rospy.is_shutdown():
+            if self.disabled:
+                if time.time() > self.disable_stop:
+                    print "lights no longer disabled!"
+                    self.disabled = False
+
             msg = Data()
             msg.source = identity.get_spot_name()
             msg.timestamp = rospy.Time().now()
             msg.property = "lights_brightness"
-            msg.value = self.brightness
+            if self.disabled:
+                msg.value = 0.0
+            else:
+                msg.value = self.brightness
             self.pub.publish(msg)
             print "Published lights brightness:", msg.value
 
@@ -54,6 +83,14 @@ class LightController(object):
             msg.value = 1.0 if self.whites_on else 0.0
             self.pub.publish(msg)
             print "Published lights whites on:", msg.value
+
+            if self.disabled:
+                diff = (self.disable_stop - time.time()) / 60.0
+                alertmsg = Alert()
+                alertmsg.spot_id = identity.get_spot_name()
+                alertmsg.timestamp = rospy.Time.now()
+                alertmsg.alert_text = "Lights disabled for another %.1f minutes" % diff
+                self.alert_pub.publish(alertmsg)
 
             time.sleep(PUB_DELAY)
 
