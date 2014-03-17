e38777994d2296ced94201896163ce8625f8c2f6
SPOT-2 <dane.t.larsen@gmail.com>
2014-03-17 22:03:18 +0000

    adding motor file

--- curtain_motor/motor.py
+++ curtain_motor/motor.py
@@ -15,52 +15,93 @@ LOW =  "0"
 INPUT = "0"
 OUTPUT = "1"
 
-while True:
-    ## First, populate the arrays with file objects that we can use later.
-    for i in range(2,4):
-        pinMode.append(os.path.join(GPIO_MODE_PATH, 'gpio'+str(i)))
-        pinData.append(os.path.join(GPIO_PIN_PATH, 'gpio'+str(i)))
+#while True:
+## First, populate the arrays with file objects that we can use later.
+for i in range(0,5):
+    pinMode.append(os.path.join(GPIO_MODE_PATH, 'gpio'+str(i)))
+    pinData.append(os.path.join(GPIO_PIN_PATH, 'gpio'+str(i)))
 
-    ## Make all the pins outputs
-    for pin in pinMode:
-        file = open(pin, 'r+')  ## open the file in r/w mode
-        file.write(OUTPUT)      ## set the mode of the pin
-        file.close()            ## IMPORTANT- must close file to make changes!
+## Make all the pins outputs
+for pin in pinMode:
+    file = open(pin, 'r+')  ## open the file in r/w mode
+    file.write(OUTPUT)      ## set the mode of the pin
+    file.close()            ## IMPORTANT- must close file to make changes!
+
+## Make all the pins low
+#for pin in pinData:
+file = open(pinData[0], 'r+') ##open the file in r/w mode
+file.write(LOW)        ## set the pin to LOW
+file.close()           ## IMPORTANT - must close file to make changes!
+
+file = open(pinData[1], 'r+') ##open the file in r/w mode
+file.write(HIGH)        ## set the pin to LOW
+file.close()           ## IMPORTANT - must close file to make changes!
+
+file = open(pinData[2], 'r+') ##open the file in r/w mode
+file.write(LOW)        ## set the pin to LOW
+file.close() 
+
+file = open(pinData[3], 'r+') ##open the file in r/w mode
+file.write(HIGH)        ## set the pin to LOW
+file.close()           ## IMPORTANT - must close file to make changes!
+
+file = open(pinData[4], 'r+') ##open the file in r/w mode
+file.write(LOW)        ## set the pin to LOW
+file.close()           ## IMPORTANT - must close file to make changes!
+
+from array import *
+temp = ['','','','','']
+file = open(pinData[0], 'r')
+temp[0] = file.read()
+
+file = open(pinData[1], 'r')
+temp[1] = file.read()
+
+file = open(pinData[2], 'r')
+temp[2] = file.read()
+
+file = open(pinData[3], 'r')
+temp[3] = file.read()
+
+file = open(pinData[4], 'r')
+temp[4] = file.read()
+
+print"" + temp[0] + temp[1] + temp[2] + temp[3] + temp[4]
 
     ##Rotate the left motor in Clockwise direction
-    file = open(pinData[2], 'r+') ## accessing pin 2 data file 
-    file.write(HIGH)      ## set the mode of the pin
-    file.close()          ## IMPORTANT- must close file to make changes!
+    #file = open(pinData[3], 'r+') ## accessing pin 2 data file 
+    #file.write(HIGH)      ## set the mode of the pin
+    #file.close()          ## IMPORTANT- must close file to make changes!
 
-    file = open(pinData[3], 'r+') ## accessing pin 2 data file 
-    file.write(LOW)       ## set the mode of the pin
-    file.close()          ## IMPORTANT- must close file to make changes!
+    #file = open(pinData[4], 'r+') ## accessing pin 2 data file 
+    #file.write(LOW)       ## set the mode of the pin
+    #file.close()          ## IMPORTANT- must close file to make changes!
 
-    time.sleep(0.25)  
+    #time.sleep(0.25)  
 
     ##Brake the rotation
-    file = open(pinData[2], 'r+') ## accessing pin 2 data file 
-    file.write(LOW)       ## set the mode of the pin
-    file.close()          ## IMPORTANT- must close file to make changes!
+    #file = open(pinData[3], 'r+') ## accessing pin 2 data file 
+    #file.write(LOW)       ## set the mode of the pin
+    #file.close()          ## IMPORTANT- must close file to make changes!
 
-    time.sleep(0.1)
+    #time.sleep(0.1)
 
     ##Rotate the left motor in Anti-Clockwise direction
-    file = open(pinData[2], 'r+') ## accessing pin 2 data file 
-    file.write(LOW)       ## set the mode of the pin
-    file.close()          ## IMPORTANT- must close file to make changes!
+    #file = open(pinData[3], 'r+') ## accessing pin 2 data file 
+    #file.write(LOW)       ## set the mode of the pin
+    #file.close()          ## IMPORTANT- must close file to make changes!
 
-    file = open(pinData[3], 'r+') ## accessing pin 2 data file 
-    file.write(HIGH)      ## set the mode of the pin
-    file.close()          ## IMPORTANT- must close file to make changes!
+    #file = open(pinData[4], 'r+') ## accessing pin 2 data file 
+    #file.write(HIGH)      ## set the mode of the pin
+    #file.close()          ## IMPORTANT- must close file to make changes!
 
-    time.sleep(0.25)
+    #time.sleep(0.25)
     
     ##Brake the rotation
-    file = open(pinData[3], 'r+') ## accessing pin 2 data file 
-    file.write(LOW)       ## set the mode of the pin
-    file.close()          ## IMPORTANT- must close file to make changes!
+    #file = open(pinData[4], 'r+') ## accessing pin 2 data file 
+    #file.write(LOW)       ## set the mode of the pin
+    #file.close()          ## IMPORTANT- must close file to make changes!
 
-    time.sleep(0.1)
+    #time.sleep(0.5)
 
 
