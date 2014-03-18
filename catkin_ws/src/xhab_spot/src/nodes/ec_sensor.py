ce2bac7e36de4e9389ba236f0e3374a541cf7a79
mira67 <dane.t.larsen@gmail.com>
2014-03-18 22:42:15 +0000

    ec node now reads from sensor

--- catkin_ws/src/xhab_spot/src/nodes/ec_sensor.py
+++ catkin_ws/src/xhab_spot/src/nodes/ec_sensor.py
@@ -4,31 +4,54 @@ import sys
 import rospy
 from xhab_spot.msg import *
 import identity
-
+import time
 import random
+import serial
+import ecwrapper
+
+PUB_DELAY = 15
 
 class ECSensor(object):
     def __init__(self):
         print "ECSensor init"
         rospy.init_node("ECSensor")
         subtopic = "/tasks/" + identity.get_spot_name() + "/ec"
-        pubtopic = "/data/" + identity.get_spot_name() + "/ec"
-        self.pub = rospy.Publisher(pubtopic, Data)
+        ecpubtopic = "/data/" + identity.get_spot_name() + "/ec"
+        self.ecpub = rospy.Publisher(ecpubtopic, Data)
+        waterpubtopic = "/data/" + identity.get_spot_name() + "/water_temperature"
+        self.waterpub = rospy.Publisher(waterpubtopic, Data)
         self.sub = rospy.Subscriber(subtopic, ECTask, self.callback)
+        self.ec_reading = 0.0
+        self.water_temp = 0.0
+        
 
     def callback(self, msg):
         print "got msg, target =", msg.target
-        reading = random.normalvariate(7, 1)
-        pubmsg = Data()
-        pubmsg.source = identity.get_spot_name()
-        pubmsg.property = "ec_reading"
-        pubmsg.timestamp = rospy.Time.now()
-        pubmsg.value = reading
-        self.pub.publish(pubmsg)
+        gpio_pin = 6
+        Apin1 = 2
+        Apin2 = 3
+        self.ec_reading, self.water_temp = ecwrapper.sensorData_read(gpio_pin, Apin1, Apin2)
+        
+        print "read EC value:", self.ec_reading
+        print "read water temp:", self.water_temp
 
     def spin(self):
         print "ECSensor listening"
-        rospy.spin()
+        while not rospy.is_shutdown():
+            pubmsg = Data()
+            pubmsg.source = identity.get_spot_name()
+            pubmsg.property = "ec_reading"
+            pubmsg.timestamp = rospy.Time.now()
+            pubmsg.value = self.ec_reading
+            self.ecpub.publish(pubmsg)
+            print "Published EC:", self.ec_reading
+
+            pubmsg.property = "water_temperature"
+            pubmsg.value = self.water_temp
+            self.waterpub.publish(pubmsg)
+            print "Published water temp:", self.water_temp
+            time.sleep(PUB_DELAY)
+
 
 if __name__ == "__main__":
     try:
