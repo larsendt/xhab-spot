15396d304c4e8a7e0ac94c7557ef2d0fd91a5e3c
SPOT-2 <dane.t.larsen@gmail.com>
2014-03-24 20:42:33 +0000

    Water Level Changes

--- camera/captureFrame.py
+++ camera/captureFrame.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python
 
-#press ESC key to exit
-#press "t" to display the time and date of frame
-#pass "-timestamp" or "--t" commandline arg to store frames with timestamp name format
-#if no args passed, frames are stored in serial number name format
-	
 import cv
 import sys, time, datetime
 
-cv.NamedWindow('video_stream')
 #put the device number here. Can be found by running 'ls /dev/video*' command.
 #if only one camera is connected to pc, then it is set to 0
 #in case of laptop, device 0 corresponds to integrated webcam
@@ -18,40 +12,29 @@ if not capture:
    print "Error opening Device"
    sys.exit()
 
-img_cnt = 0
-dt = 0
-
-def argcheck():
-        if len(sys.argv) > 1:
-           firstarg = str(sys.argv[1])
-           return (firstarg == "-timestamp" or firstarg == "--t")
-        else: return 0
-
-def capture_stream():
+def capture_stream(path):
         frame = cv.QueryFrame(capture)
         stamp = time.time()
         if frame is None:
-                sys.exit()
+               print "no frame"
+               sys.exit()
         cv.Flip(frame, None, 1)
-        cv.ShowImage('video_stream', frame)
         # the image extension can be changed to desired format. replace .ppm with .jpg/.jpeg/.png etc
-        if argcheck():
-           image = "image_"+`stamp`+".ppm"
-        else:
-           image = "image_"+`img_cnt`+".ppm"
-        cv.SaveImage(image, frame)
-        return stamp
-   
-while 1:
-        stamp = capture_stream()
-        img_cnt = img_cnt+1
+        image_name = "image_"+`stamp`+".ppm"
+        cv.SaveImage(path+image_name, frame)
+        return image_name
+
+# the following while loop is for testing purpose. 
+# uncomment the while loop to see how the capture_stream() can be used.
+
+#while 1:
+#        image = capture_stream("/home/xhab/xhab-spot/camera/")
+#        prefix,stamp = image.split("_", 1)
+#        stamp,postfix = stamp.split(".ppm", 1)
+#        print stamp+"  "+`datetime.datetime.fromtimestamp(float(stamp)).strftime('%Y-%m-%d_%H:%M:%S')`
         #the delay mentioned below can be varied
         #for now keeping this, so that less images will be generated in the given amount of time
-        key = cv.WaitKey(50)
-        if key == 27: # press ESC key to exit
-           break
-        elif key == 116: #press "t" to display the time and date of frame
-           dt = datetime.datetime.fromtimestamp(stamp).strftime('%Y-%m-%d_%H:%M:%S')
-           print dt
-
+#        key = cv.WaitKey(50)
+#        if key == 27: # press ESC key to exit
+#           break
 
