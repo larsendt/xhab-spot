a7dfc898c72daa48efb3d894e20129734511c04f
SPOT-2 <dane.t.larsen@gmail.com>
2014-03-27 22:16:14 +0000

    made ros node for humidity, still need actual sensor

--- catkin_ws/src/xhab_spot/src/nodes/humidity_sensor.py
+++ catkin_ws/src/xhab_spot/src/nodes/humidity_sensor.py
@@ -15,7 +15,7 @@ class HumiditySensor(object):
     def __init__(self):
         print "HumiditySensor init"
         rospy.init_node("HumiditySensor")
-        subtopic = "/tasks/" + identity.get_spot_name() + "/ec"
+        subtopic = "/tasks/" + identity.get_spot_name() + "/humidity"
         humiditypubtopic = "/data/" + identity.get_spot_name() + "/humidity"
         self.humiditypub = rospy.Publisher(humiditypubtopic, Data)
         airpubtopic = "/data/" + identity.get_spot_name() + "/air_temperature"
@@ -27,29 +27,23 @@ class HumiditySensor(object):
 
     def callback(self, msg):
         print "got msg, target =", msg.target
-        gpio_pin = 6
-        Apin1 = 2
-        Apin2 = 3
-        self.ec_reading, self.water_temp = ecwrapper.sensorData_read(gpio_pin, Apin1, Apin2)
         
-        print "read EC value:", self.ec_reading
-        print "read water temp:", self.water_temp
 
     def spin(self):
         print "HumiditySensor listening"
         while not rospy.is_shutdown():
             pubmsg = Data()
             pubmsg.source = identity.get_spot_name()
-            pubmsg.property = "ec_reading"
+            pubmsg.property = "humidity_reading"
             pubmsg.timestamp = rospy.Time.now()
-            pubmsg.value = self.ec_reading
-            self.ecpub.publish(pubmsg)
-            print "Published EC:", self.ec_reading
+            pubmsg.value = self.humidity_reading
+            self.humiditypub.publish(pubmsg)
+            print "Published humidity:", self.humidity_reading
 
-            pubmsg.property = "water_temperature"
-            pubmsg.value = self.water_temp
-            self.waterpub.publish(pubmsg)
-            print "Published water temp:", self.water_temp
+            pubmsg.property = "air_temperature"
+            pubmsg.value = self.air_temp
+            self.airpub.publish(pubmsg)
+            print "Published air temp:", self.air_temp
             time.sleep(PUB_DELAY)
 
 
