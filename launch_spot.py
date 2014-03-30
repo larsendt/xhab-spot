f044d74c05871f655e3dbf81b257821bbf542dc2
SPOT-2 <dane.t.larsen@gmail.com>
2014-03-30 18:45:41 +0000

    launch spot script now has daemon mode

--- launch_spot.py
+++ launch_spot.py
@@ -6,18 +6,21 @@ import sys
 import os
 import signal
 import psutil
+import daemon
+import argparse
 from threading  import Thread
 from Queue import Queue, Empty
 
-SERVICE_DIR = "service_files/"
+SERVICE_DIR = "/home/xhab/xhab-spot/service_files/"
 
 if not os.path.exists(SERVICE_DIR):
     os.makedirs(SERVICE_DIR)
 
 class Service(object):
-    def __init__(self, cmd):
+    def __init__(self, cmd, name):
         self.cmd = cmd.split(" ")
         self.cmdline = cmd
+        self.name = name
         self.proc = None
         self.stdout_queue = None
         self.stderr_queue = None
@@ -28,8 +31,8 @@ class Service(object):
         self.is_alive = False
 
     def run(self):
-        print self.cmdline
         self.proc = sp.Popen(self.cmd, stdout=sp.PIPE, stderr=sp.PIPE, close_fds=True)
+        print "Launch: %s [pid=%d]" % (self.name, self.proc.pid)
         self.stdout_queue = Queue()
         self.stdout_thread = Thread(target=self.enqueue_output, args=(self.proc.stdout, self.stdout_queue))
         self.stdout_thread.daemon = True
@@ -42,6 +45,7 @@ class Service(object):
 
     def kill(self):
         try:
+            print "Kill: %s [pid=%d]" % (self.name, self.proc.pid)
             self.proc.kill()
             self.proc.wait()
         except:
@@ -95,31 +99,31 @@ def start_roscore():
             if "rosmaster" in item or "roscore" in item:
                 print "ROS Core is already running"
                 return None
-    print "Starting ROS Core"
-    return sp.Popen("roscore", close_fds=True, stdout=sp.PIPE, stderr=sp.PIPE)
+    proc = sp.Popen("roscore", close_fds=True, stdout=sp.PIPE, stderr=sp.PIPE)
+    print "Starting ROS Core [pid=%d]" % proc.pid
+    return proc
 
 
 def stop_roscore(roscore_proc):
     if roscore_proc:
-        proc = psutil.Process(roscore_proc.pid)
-        print "Killing ROS Core:", proc
+        print "Killing ROS Core [pid=%d]" % roscore_proc.pid
         roscore_proc.kill()
         roscore_proc.wait()
     else:
         for proc in psutil.process_iter():
             for item in proc.cmdline:
                 if "roscore" in item or "rosmaster" in item:
-                    print "Killing ROS Core:", proc
+                    print "Killing ROS Core [pid=%d]" % proc.pid
                     os.kill(proc.pid, signal.SIGKILL)
 
 
 def start_lcd_driver():
-    print "Starting LCD driver"
     proc = sp.Popen(["python", "/home/xhab/xhab-spot/spot_lib/lcd_menu.py"], close_fds=True, stdout=sp.PIPE, stderr=sp.PIPE)
+    print "Starting LCD Driver [pid=%d]" % proc.pid
     return proc
 
 def stop_lcd_driver(proc):
-    print "Killing lcd interface:", psutil.Process(proc.pid)
+    print "Killing LCD Driver [pid=%d]" % proc.pid
     proc.kill()
     proc.wait()
     for p in psutil.process_iter():
@@ -144,7 +148,10 @@ ROS_NODES = ["battery_node.py",
              "rotation_controller.py"]
 
 CMDS = map(lambda x: "rosrun xhab_spot " + x, ROS_NODES)
-SERVICES = map(lambda x: Service(x), CMDS)
+SERVICES = map(lambda (cmd, name): Service(cmd, name), zip(CMDS, ROS_NODES))
+ROSCORE_PROC = None
+LCD_PROC = None
+ROSMASTER_IP = None
 
 def check_already_running(interactive):
     otherproc = None
@@ -155,10 +162,9 @@ def check_already_running(interactive):
                 break
 
     if otherproc is None:
-        return
+        return False
     
-    print "Found another SPOT process:", proc.pid
-    print "Self is:", os.getpid()
+    print "Found another SPOT process: \"%s\" [pid=%d]" % (" ".join(proc.cmdline), proc.pid)
     if interactive:
         print "Type 'y' to shut down other process, 'n' to exit"
         inp = ""
@@ -175,42 +181,34 @@ def check_already_running(interactive):
         print "Exiting"
         sys.exit(0)
 
-
-def usage():
-    print """Usage: %s <opts>
-Options:
-    --no-interactive              Don't prompt the user (will auto-kill any 
-                                  other SPOT instances)
-    --remote-rosmaster <ip addr>  Don't launch a local ROS master, instead 
-                                  use one on specified IP address""" % (sys.argv[0])
+    return True
 
 
-def main():
-    local_rosmaster = True
-    interactive = True
-    if len(sys.argv) == 2 and sys.argv[1] == "--no-interactive":
-        interactive = False
-    elif len(sys.argv) == 3 and sys.argv[1] == "--remote-rosmaster":
-        local_rosmaster = False
-    elif len(sys.argv) > 1:
-        usage()
-        return 
+def terminate_services():
+    map(lambda x: x.kill(), SERVICES)
+    if ROSMASTER_IP is None:
+        stop_roscore(ROSCORE_PROC)
+    stop_lcd_driver(LCD_PROC)
 
-    # check to see if another instance is already running
-    check_already_running(interactive)
+sys.excepthook = terminate_services
 
-    if local_rosmaster:
+
+def main(rosmaster_ip):
+    if rosmaster_ip is None:
         # launch a local ros master
         roscore_proc = start_roscore()
+        global ROSCORE_PROC
+        ROSCORE_PROC = roscore_proc
     else:
         # use a remote ros master with the specified ip
-        ip = sys.argv[2]
-        uri = "http://%s:11311" % ip
+        uri = "http://%s:11311" % rosmaster_ip
         print "ROS_MASTER_URI is:", uri
         os.putenv("ROS_MASTER_URI", uri)
 
     # start the LCD driver stuff
     lcd_proc = start_lcd_driver()
+    global LCD_PROC
+    LCD_PROC = lcd_proc
 
 
     print "Launching %d services" % len(SERVICES)
@@ -222,12 +220,43 @@ def main():
             map(lambda x: x.update(), SERVICES)
     except KeyboardInterrupt:
         print "Terminating all services"
-        map(lambda x: x.kill(), SERVICES)
-        if local_rosmaster:
-            stop_roscore(roscore_proc)
-        stop_lcd_driver(lcd_proc)
+        terminate_services()
         print "Done"
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Launch SPOT")
+    parser.add_argument("--no-interactive", help="Don't prompt the user for anything.", action="store_true")
+    parser.add_argument("--remote-rosmaster", metavar="ip addr", help="IP address for a remote ROS master instance. If this is not specified, a local ROS Master will be launched.", nargs=1)
+    parser.add_argument("--daemon", help="Launch a daemon process in the background.", action="store_true")
+    parser.add_argument("--kill", help="Stop an existing SPOT instance, but don't launch another", action="store_true")
+    args = parser.parse_args()
+    if args.remote_rosmaster:
+        rosmaster_ip = args.remote_rosmaster[0]
+    else:
+        rosmaster_ip = None
+
+    ROSMASTER_IP = rosmaster_ip
+    interactive = not args.no_interactive
+
+    # check to see if another instance is already running
+    killed = check_already_running(interactive)
+
+    if args.kill:
+        if not killed:
+            print "No SPOT instance found."
+        sys.exit(0)
+
+
+    if args.daemon:
+        logfile = open("/home/xhab/spot.log", "w")
+        context = daemon.DaemonContext(stdout=logfile, stderr=logfile)
+        print "SPOT instance is now running in daemon mode. Use %s --kill to stop the instance" % sys.argv[0] 
+        with context:
+            main(rosmaster_ip)
+    else:
+        main(rosmaster_ip)
+
+
+
+
