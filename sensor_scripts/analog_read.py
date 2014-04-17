cb204744b6442de163a0b0b222bd9f0ae78836ce
SPOT-4 <dane.t.larsen@gmail.com>
2014-04-17 22:11:57 +0000

    analog code

--- sensor_scripts/analog_read.py
+++ sensor_scripts/analog_read.py
@@ -10,7 +10,8 @@ import time
 fname = "/proc/adc2"
 
 while 1:
-	with open(fname, "r") as f:
-		val = int(f.read()[5:-1])
-		print str(val) + "   " + ("=" * (val / 25))
-		time.sleep(0.05)
+    with open(fname, "r") as f:
+        val = int(f.read()[5:-1])
+        frac = "%.1f%%" % (100 * (float(val) / 4095))
+        print str(val) + " " + str(frac) + "   " + ("=" * (val / 25))
+        time.sleep(0.1)
